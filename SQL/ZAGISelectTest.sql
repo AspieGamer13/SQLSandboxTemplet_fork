57cd10cf0aa8c858cc7ae40b83a39f17073d0aae
Selah <95000468+AspieGamer13@users.noreply.github.com>
2024-01-31 02:00:21 +0000

    20240130 In Class

---
+++ SQL/ZAGISelectTest.sql
@@ -0,0 +1,11 @@
+USE ZAGIDB;
+
+select * from product--;
+
+--SELECT productid, productname, productprice, productprice * 2 as 'double price' from product where productid > '1x1';
+
+--select distinct vendorid from product;
+
+--select count(distinct vendorid) from product;
+
+ORDER BY productprice;
